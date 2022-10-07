01d26251affd460fad83eab2c3776cc7576063a5
Yelf-0h <2817857373@qq.com>
2022-10-07 17:27:52 +0800

    添加了角色限制.hasrole等，添加了403错误页面的指定 靠http.exceptionHandler.accessDeniedPage设置 添加了注解式操作

--- src/main/java/edu/yecheng/springsecurity/controller/TestController.java
+++ src/main/java/edu/yecheng/springsecurity/controller/TestController.java
@@ -1,9 +1,14 @@
 package edu.yecheng.springsecurity.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.yecheng.springsecurity.entity.Users;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Yelf
@@ -15,12 +20,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello-security";
     }
 
     @GetMapping("/index")
-    public String index(){
+    public String index() {
         return "hello-index";
     }
+
+    @GetMapping("/update")
+//    @Secured({"ROLE_jack","ROLE_yecheng"})  //角色校验注解
+//    @PreAuthorize("hasAuthority('admins')")  //权限校验注解，在方法执行之前校验
+    @PostAuthorize("hasAuthority('admin')")  //权限校验注解，在方法执行之后校验，所以就算没权限也是会执行方法
+    public String update() {
+        System.out.println("update....................");
+        return "hello-update";
+    }
+
+    @GetMapping("/getAll")
+    @PostAuthorize("hasAuthority('admin')")
+    @PostFilter("filterObject.username == 'admin1'")  //权限验证之后对数据进行过滤 留下用户名是 admin1 的数据
+    public List<Users> getAllUser() {
+
+        ArrayList<Users> list = new ArrayList<>();
+        list.add(new Users(1, "admin1", "6666"));
+        list.add(new Users(2, "admin2", "888"));
+        System.out.println(list);
+        return list;
+    }
+
 }
